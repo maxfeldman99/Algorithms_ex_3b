06daa5ac23862ce54a6b232e78b02c9d1d5e588d
MAX FELDMAN <maxfeldman99@gmail.com>
2018-05-18 02:58:25 +0300

    reading from file is working , creating a dynamic graph based on file data - is working.

--- Algorithms_ex_3b/p1.cpp
+++ Algorithms_ex_3b/p1.cpp
@@ -6,82 +6,107 @@
 #include <vector>
 #include <list>
 
+
+
 using namespace std;
+
+typedef struct Vertex {
+
+	int currentVertex;
+	vector<int> myEdges;
+
+} Vertex;
+
+
+
+// This class represents a directed graph using
+// adjacency list representation
+class Graph
+{
+	int V;    // No. of vertices
+
+			  // Pointer to an array containing adjacency
+			  // lists
+	list<int> *adj;
+public:
+	Graph(int V);  // Constructor
+
+				   // function to add an edge to graph
+	void addEdge(int v, int w);
+
+	// prints BFS traversal from a given source s
+	void BFS(int s);
+};
+
 
 string fileNameInput();
 int getNumOfVertices(ifstream &myfile);
-void getEdges(ifstream &myfile, string fname);
+Vertex* getEdges(ifstream &myfile, string fname, int numOfVertices);
+Graph createAndFillGraph(Vertex *arrayOfEdges, int numOfVertices);
+void getNumOfShortestPaths(Graph g);
 
 
-//// This class represents a directed graph using
-//// adjacency list representation
-//class Graph
-//{
-//	int V;    // No. of vertices
-//
-//			  // Pointer to an array containing adjacency
-//			  // lists
-//	list<int> *adj;
-//public:
-//	Graph(int V);  // Constructor
-//
-//				   // function to add an edge to graph
-//	void addEdge(int v, int w);
-//
-//	// prints BFS traversal from a given source s
-//	void BFS(int s);
-//};
-//
-//Graph::Graph(int V)
-//{
-//	this->V = V;
-//	adj = new list<int>[V];
-//}
-//
-//void Graph::addEdge(int v, int w)
-//{
-//	adj[v].push_back(w); // Add w to v’s list.
-//}
-//
-//
-//void Graph::BFS(int s)
-//{
-//	// Mark all the vertices as not visited
-//	bool *visited = new bool[V];
-//	for (int i = 0; i < V; i++)
-//		visited[i] = false;
-//
-//	// Create a queue for BFS
-//	list<int> queue;
-//
-//	// Mark the current node as visited and enqueue it
-//	visited[s] = true;
-//	queue.push_back(s);
-//
-//	// 'i' will be used to get all adjacent
-//	// vertices of a vertex
-//	list<int>::iterator i;
-//
-//	while (!queue.empty())
-//	{
-//		// Dequeue a vertex from queue and print it
-//		s = queue.front();
-//		cout << s << " ";
-//		queue.pop_front();
-//
-//		// Get all adjacent vertices of the dequeued
-//		// vertex s. If a adjacent has not been visited, 
-//		// then mark it visited and enqueue it
-//		for (i = adj[s].begin(); i != adj[s].end(); ++i)
-//		{
-//			if (!visited[*i])
-//			{
-//				visited[*i] = true;
-//				queue.push_back(*i);
-//			}
-//		}
-//	}
-//}
+
+
+
+
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w)
+{
+	adj[v].push_back(w); // Add w to v’s list.
+}
+
+
+void Graph::BFS(int s)
+{
+	// Mark all the vertices as not visited
+	bool *visited = new bool[V];
+	for (int i = 0; i < V; i++)
+		visited[i] = false;
+
+	// Create a queue for BFS
+	list<int> queue;
+
+	// Mark the current node as visited and enqueue it
+	visited[s] = true;
+	queue.push_back(s);
+
+	// 'i' will be used to get all adjacent
+	// vertices of a vertex
+	list<int>::iterator i;
+
+	while (!queue.empty())
+	{
+		// Dequeue a vertex from queue and print it
+		s = queue.front();
+		cout << s << " ";
+		queue.pop_front();
+
+		// Get all adjacent vertices of the dequeued
+		// vertex s. If a adjacent has not been visited, 
+		// then mark it visited and enqueue it
+		for (i = adj[s].begin(); i != adj[s].end(); ++i)
+		{
+			if (!visited[*i])
+			{
+				visited[*i] = true;
+				queue.push_back(*i);
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
 
 int getNumOfVertices(ifstream &myfile) {
 	int numberOfVertices;
@@ -105,14 +130,11 @@ int getNumOfVertices(ifstream &myfile) {
 		}
 
 	}
-	myfile.close();
+	//myfile.close();
 
 }
 
-//void fillGraphWithData(Graph graph) {
-//
-//
-//}
+
 string fileNameInput() {
 	string fname;
 	cout << "Please enter a name of an input file: " << endl;
@@ -120,37 +142,104 @@ string fileNameInput() {
 	return fname;
 }
 
-void getEdges(ifstream &myfile,string fname) {
-	//myfile.open(fname, ios::in);
+Vertex* getEdges(ifstream &myfile,string fname,int numOfVertices) {
+	myfile.open(fname);
 	char my_character;
 	int number_of_lines = 0;
 	vector<int> myEdge;
-
-
-	while (!myfile.eof()) {
-
-		myfile >> my_character;
-
-		//myfile.get(my_character);
-		cout << my_character;
+	int i = 0;
+	string numToParse;
+	int vertexToInsert;
+	Vertex *arrayOfEdges = new Vertex[numOfVertices];
+	vector<int>::const_iterator j;
 	
-		//if (my_character == '\n') {
-		//	++number_of_lines;
-		//}
-		//if (my_character == '{') { // detects Edge
-		//	
-		//	while (!my_character == '}') {
-		//		myfile.get(my_character);
-		//		if (my_character != ',') {
-		//			cout << my_character << endl;
-		//		}
-		//		else {
-		//			cout << my_character << endl;
-		//		}
-		//	}
-		//}
+
+		if (!myfile.is_open()) {
+		exit(EXIT_FAILURE);
+			}
+		else {
+			while (!myfile.eof()) {
+
+				myfile >> my_character;
+
+				if (my_character == '{') { // detects new set of Edges
+					myfile >> my_character;
+					
+					while (my_character != '}') {
+						if (my_character != ',' ) {
+							stringstream iss;
+							arrayOfEdges[i].myEdges.reserve(numOfVertices - 1);
+							arrayOfEdges[i].currentVertex = i + 1;
+							iss << my_character;
+							iss >> vertexToInsert;
+							arrayOfEdges[i].myEdges.push_back(vertexToInsert);
+							//	cout << "this is a num :";
+							//cout << "the index is : " << i + 1 << endl;
+							//cout << vertexToInsert << endl;
+							
+						}
+						myfile >> my_character;
+						
+					}
+					i++;
+				}
+			}
+
+		}
+
+	
+	for (int i = 0; i < numOfVertices; i++) {
+		
+		if (arrayOfEdges[i].currentVertex > 0) {  // otherwise the system shows the currentVertex as a negative number( memory not allocated)
+			cout << "current vertex is : " << arrayOfEdges[i].currentVertex << endl; ;
+			for (j = arrayOfEdges[i].myEdges.begin(); j != arrayOfEdges[i].myEdges.end(); ++j) {
+
+				cout << (*j) << ",";
+			}
+			cout << endl;
+		}
 	}
-	cout << "NUMBER OF LINES: " << number_of_lines << endl;
+
+	return arrayOfEdges;
+
+}
+
+Graph createAndFillGraph(Vertex *arrayOfEdges, int numOfVertices) {
+	
+	Graph g(numOfVertices);
+	vector<int>::const_iterator j;
+	int k = 0;
+	int v1, v2;
+	for (int i = 0; i < numOfVertices; i++) {
+		if (arrayOfEdges[k].myEdges.size() > 0) {
+			v1 = arrayOfEdges[k].currentVertex;
+			for (j = arrayOfEdges[i].myEdges.begin(); j != arrayOfEdges[i].myEdges.end(); ++j) {
+				
+				v2 = (*j);
+				g.addEdge(v1, v2);
+			}
+		}
+		k++;
+	}
+
+
+	
+	return g;
+}
+void getNumOfShortestPaths(Graph g) {
+	int startVertex;
+	char answer;
+	cout << "Please enter the source node : " << endl;
+	cin >> startVertex;
+	cout << "The output for " << startVertex << " is : " << endl;
+	g.BFS(startVertex);
+	cout << endl;
+	cout << "Do you want to continue (y / n) : " << endl;
+	cin >> answer;
+	if (answer == 'y') {
+		getNumOfShortestPaths(g);
+	}
+
 
 }
 
@@ -162,32 +251,16 @@ void main() {
 
 	fname = fileNameInput();
 	myfile.open(fname);
+	int numOfVertices = getNumOfVertices(myfile);
+	myfile.close();
+	Vertex* arrayOfEdges = getEdges(myfile, fname,numOfVertices);
+	Graph g = createAndFillGraph(arrayOfEdges, numOfVertices);
+	getNumOfShortestPaths(g);
 	
-	getNumOfVertices(myfile);
-	myfile.clear();
-	getEdges(myfile, fname);
-	//getVertices();
+	
 
 
-	//fillGraphWithData();
-	cout << "Please enter the source node : " << endl;
-	cin >> startVertex;
-	cout << "The output for " << startVertex << " is : " << endl;
-	//printAnswer();
-
-
-	/*Graph g(4);
-
-	g.addEdge(0, 1);
-	g.addEdge(0, 2);
-	g.addEdge(1, 2);
-	g.addEdge(2, 0);
-	g.addEdge(2, 3);
-	g.addEdge(3, 3);
-
-	cout << "Following is Breadth First Traversal "
-		<< "(starting from vertex 2) \n";
-	g.BFS(2);*/
+	
 
 	system("pause");
 
